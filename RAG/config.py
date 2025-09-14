67f65c446eb4946339bac652dba93b76fbe970ed
ChowchowWorks <clarencechow1@outlook.com>
2025-09-14 21:33:21 +0800

    RAG update

--- RAG/config.py
+++ RAG/config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 HF_TOKEN = os.getenv("HF_API_TOKEN")
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 DOCS_DIR = os.path.join(PROJECT_ROOT, "Documents")
-CHROMA_DB_DIR = os.path.join(PROJECT_ROOT, "chroma_db")
+CHROMA_DB_DIR = os.path.join(PROJECT_ROOT, "RAG/chroma_db")
